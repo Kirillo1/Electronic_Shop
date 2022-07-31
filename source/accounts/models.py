2824d7ee4ed4e1220e339ce14b3d6eaa5c0ed968
ghost <kirill.han011@gmail.com>
2022-07-31 13:42:24 +0600

    Добавлена модель профиля, созданы и применены миграции

--- source/accounts/models.py
+++ source/accounts/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(get_user_model(), related_name="profile",
+                                verbose_name="Профиль", on_delete=models.CASCADE)
+    addi_info = models.TextField(max_length=3000, verbose_name="Дополнительная информация", null=True, blank=True)
+
+
+class Meta:
+    verbose_name = "Profile"
+    verbose_name_plural = "Profiles"
+
+
+def __str__(self) -> str:
+    return f"Profile: {self.user.username}. {self.id}"
+
